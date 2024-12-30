1021a4128f2579b7e846e0bc2a06daf1a154f4e5
helper <helper@help.com>
2024-12-30 17:38:03 +0300

    payment 3

--- src/components/PatientRegPayment.js
+++ src/components/PatientRegPayment.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import BookingPaymentCor from "./BookingPaymentCor";
-import { Autocomplete, Stack, TextField, Typography, InputAdornment } from "@mui/material";
+import { Autocomplete, Stack, TextField, Typography, InputAdornment, Button } from "@mui/material";
 
 export default function PatientRegPayment({listSelectedServices}){
     const [discountPercent, setDiscountPercent] = useState(0)
     const [discounter, setDiscounter] = useState(null)
+    
     const DISCOUNTERS = ['Dr. 111', 'Owner']
+
     return <>
-        <BookingPaymentCor isDiscounterOn={true} discountPercent={discountPercent} listSelectedServices={listSelectedServices}/>
-        <Stack direction={'row'}>
-            <Typography variant="h4">Discounts</Typography>
-            <Stack direction={'column'}>
+        <Stack direction={'column'}  p={2}>
+            <Typography variant="h6" textAlign={'start'}>Payment</Typography>
+            <BookingPaymentCor isDiscounterOn={true} discountPercent={discountPercent} listSelectedServices={listSelectedServices}/>
+        </Stack>
+        <Stack direction={'column'} p={2}>
+            <Typography variant="h6" textAlign={'start'}>Discounts</Typography>
+            <Stack direction={'row'} spacing={2} p={2}>
                 <Autocomplete
                     value={discounter}
                     onChange={(event,newvalue)=>{
@@ -18,13 +23,14 @@ export default function PatientRegPayment({listSelectedServices}){
                         }}
                     id="discounters_choice"
                     options={DISCOUNTERS}
-                    renderInput={(params)=><TextField {...params} label={'Discounted by:'}/>}
+                    renderInput={(params)=><TextField {...params} label={'Discounted by:'} sx={{minWidth:'200px'}} slotProps={{inputLabel:{shrink:true},}}/>}
                 />
-                <TextField variant="outlined" label="Discount Percentage" name="discountpercent" size="small" sx={{width:'150px'}}
-                    slotProps={{input:{endAdornment:<InputAdornment position="end">%</InputAdornment>}}}
+                <TextField variant="outlined" label="Discount Percentage" name="discountpercent" sx={{width:'150px'}}
+                    slotProps={{inputLabel:{shrink:true}, input:{endAdornment:<InputAdornment position="end">%</InputAdornment>}}}
+                    disabled = {discounter === null}
                     value={discountPercent} onChange={(event)=>{setDiscountPercent(event.target.value)}} />
             </Stack>
         </Stack>
-
+        <Button variant="contained">Create Payment</Button>
     </>
 }
